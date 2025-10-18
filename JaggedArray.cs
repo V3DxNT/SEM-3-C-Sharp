ce8bea446fab9b696907a83be43294dd512fac15
V3DxNT <ved4geeks@gmail.com>
2025-10-18 09:35:30 +0530

    Add : Jagged Array

---
+++ JaggedArray.cs
@@ -0,0 +1,23 @@
+using System;
+
+class JaggedArray
+{
+    static void Main()
+    {
+        int[][] jaggedArray = new int[3][];
+
+        jaggedArray[0] = new int[] { 1, 2, 3 };
+        jaggedArray[1] = new int[] { 4, 5 };
+        jaggedArray[2] = new int[] { 6, 7, 8, 9 };
+
+        for (int i = 0; i < jaggedArray.Length; i++)
+        {
+            Console.Write("Row {0}: ", i);
+            for (int j = 0; j < jaggedArray[i].Length; j++)
+            {
+                Console.Write(jaggedArray[i][j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
